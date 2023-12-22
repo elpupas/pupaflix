c0e06aaf0df43ac9fcdd18e421c5a0ec09afb97d
Peterson A Sena <petersonsena@gmail.com>
2023-12-22 20:40:04 +0100

    fix order migrations

---
+++ database/migrations/2023_12_22_193504_create_rate_series_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('rate_series', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('profile_id');
+            $table->unsignedBigInteger('serie_id');
+            $table->tinyInteger('like')->nullable();
+            $table->string('comment')->nullable();
+            $table->enum('rate', ['1', '2', '3', '4', '5'])->nullable();
+            $table->foreign('profile_id')->references('id')->on('profiles')->onDelete('cascade');
+            $table->foreign('serie_id')->references('id')->on('series')->onDelete('cascade');
+
+          
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('rate_series');
+    }
+};
