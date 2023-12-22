8997c8a8abff6a2cbe513dc0dcd827864bd425bc
Peterson A Sena <petersonsena@gmail.com>
2023-12-22 18:41:49 +0100

    create chapter model and migrations

---
+++ database/migrations/2023_12_22_170405_create_chapters_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('chapters', function (Blueprint $table) {
+            $table->id();
+            $table->string('name',100);
+            $table->string('description',250)->nullable();
+            $table->string('image',1000)->nullable();
+            $table->foreignId('season_id');
+            $table->foreign('season_id')->references('id')->on('seasons')->onDelete('cascade')->onUpdate('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('chapters');
+    }
+};
