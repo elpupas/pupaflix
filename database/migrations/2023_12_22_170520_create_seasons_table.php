ebd257135cdcc125bdb0eb5ee87e04d01f13cb9c
Peterson A Sena <petersonsena@gmail.com>
2023-12-22 18:40:11 +0100

    create season model and migration

---
+++ database/migrations/2023_12_22_170520_create_seasons_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('seasons', function (Blueprint $table) {
+            $table->id();
+            $table->string('season',90);
+            $table->string('cover-art',1000)->nullable();
+            $table->foreignId('serie_id');
+            $table->foreign('serie_id')->references('id')->on('series')->onDelete('cascade')->onUpdate('cascade');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('seasons');
+    }
+};
